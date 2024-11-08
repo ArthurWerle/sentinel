1416190277d56641f21279e1bfaa37299be2a8a6
arthurwerle <arthur.werle@gmail.com>
2024-11-08 12:07:50 -0300

    readme & deploy

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Sentinel
 ### Web crawler to look for things 
 
+#### Using AWS CLI to study devops and infra as a code.
+
 - `chmod +x deploy.sh`
 - `./deploy.sh`
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 STACK_NAME="WebCrawlerApp"
 TEMPLATE_FILE="crawler.yml"
 PYTHON_FILE="app.py"
-S3_BUCKET="crawler-lambda-code"
+S3_BUCKET="crawler-lambda-code-unique-id"
 AWS_REGION="us-east-1"
 
 # ANSI escape codes for colors and formatting
@@ -26,12 +26,15 @@ check_stack_events() {
     fi
 }
 
+echo -e "${BLUE}Setting default region to ${AWS_REGION}"
+aws configure set default.region $AWS_REGION
+
 # Create the S3 bucket if it doesn't exist
 echo -e "${BLUE}Checking if the S3 bucket exists...${NC}"
 aws s3api head-bucket --bucket $S3_BUCKET 2>/dev/null
 if [ $? -ne 0 ]; then
     echo -e "${GREEN}Bucket does not exist. Creating the bucket...${NC}"
-    aws s3api create-bucket --bucket $S3_BUCKET --region $AWS_REGION
+    aws s3api create-bucket --bucket $S3_BUCKET
     echo -e "${GREEN}Bucket created.${NC}"
 else
     echo -e "${GREEN}Bucket already exists.${NC}"
@@ -39,7 +42,7 @@ fi
 
 # Upload the Python file to S3
 echo -e "${BLUE}Uploading the Python file to S3...${NC}"
-aws s3 cp $PYTHON_FILE s3://$S3_BUCKET/$PYTHON_FILE
+aws s3 cp $PYTHON_FILE s3://$S3_BUCKET/$PYTHON_FILE 
 echo -e "${GREEN}Python file uploaded to S3.${NC}"
 
 # Check if the CloudFormation stack exists
